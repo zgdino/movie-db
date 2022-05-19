eea770baf99b81bd7bb1c1be86321e48d4900c7d
Dino Hadzic <zgdino@yahoo.com>
2022-05-19 17:52:10 +0200

    useState hooks

--- setup/src/Movies.js
+++ setup/src/Movies.js
@@ -5,6 +5,8 @@ const url =
   'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
 
 const Movies = () => {
+  const data = useGlobalContext()
+  console.log(data);
   return <h2>movies component</h2>
 }
 
--- setup/src/context.js
+++ setup/src/context.js
@@ -2,10 +2,17 @@ import React, { useState, useContext, useEffect } from 'react'
 // make sure to use https
 // instantly exporting because you will use it later somewhere else
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
-console.log(API_ENDPOINT);
+
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
+
+  const [isLoading, setIsloading] = useState(true)
+  // error is object
+  const [error, setError] = useState({show:false, msg:''})
+  const [movies, setMovies] = useState([])
+  const [query, setQuery] = useState('batman') 
+
   return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
 }
 // make sure use
