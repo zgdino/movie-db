6c7dcfc87c1014aa5defcb2d45425866cf8f45e9
Dino Hadzic <zgdino@yahoo.com>
2022-05-20 20:34:24 +0200

    setting up the individual movie card outline

--- setup/src/Movies.js
+++ setup/src/Movies.js
@@ -17,7 +17,18 @@ const Movies = () => {
         // destructuring needed properties coming from each movie
         // giving them aliases with : colon
         const { imdbID: id, Poster: poster, Title: title, Year: year } = movie
-        return <h4>movie</h4>
+
+        return (
+          <Link to={`/movies/${id}`} key={id} className='movie'>
+            <article>
+              <img src={poster} alt={title} />
+              <div className='movie-info'>
+                <h4 className='title'>{title}</h4>
+                <p>{year}</p>
+              </div>
+            </article>
+          </Link>
+        )
       })}
     </section>
   )
