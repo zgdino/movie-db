579e19b4953b6aa2c6f42c75ff18c94e00a063c0
Dino Hadzic <zgdino@yahoo.com>
2022-05-22 17:20:41 +0200

    setting up the search form

--- setup/src/SearchForm.js
+++ setup/src/SearchForm.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { useGlobalContext } from './context'
 const SearchForm = () => {
+  // pulling it from context.js
   const { query, setQuery, error } = useGlobalContext()
-  // e.prevent.default() is preventing refreshing the page every time user submits an entry
+  
   return (
+    // e.prevent.default() is preventing refreshing the page every time user submits an entry
     <form className='search-form' onSubmit={(e) => e.prevent.default()}>
       <h2>search movies</h2>
       <input
         type=''
         className='form-input'
         value={query}
+        // set the query to input provided by user
         onChange={(e) => setQuery(e.target.value)}
       />
     </form>
