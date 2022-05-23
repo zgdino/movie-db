80c1d0613c58b523be853bd5855b610cd41c23e7
Dino Hadzic <zgdino@yahoo.com>
2022-05-23 17:37:57 +0200

    single movie fetch data setup

--- setup/src/SingleMovie.js
+++ setup/src/SingleMovie.js
@@ -3,6 +3,23 @@ import { useParams, Link } from 'react-router-dom'
 import { API_ENDPOINT } from './context'
 
 const SingleMovie = () => {
+  const { id } = useParams()
+  // setting movie to be an object
+  const [movie, setMovie] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState({ show: false, msg: '' })
+
+  const fetchMovie = async (url) => {
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+  } 
+
+  useEffect(() => {
+    fetchMovie(`${API_ENDPOINT}&i=${id}`)
+    // every time id changes and it will change when movie poster/link clicked
+  }, [id])
+
   return <h2>single movie</h2>
 }
 
