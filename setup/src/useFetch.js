1346b9bc5c57364aad38a1746e4d8e0a142d8941
Dino Hadzic <zgdino@yahoo.com>
2022-05-24 17:22:34 +0200

    useFetch refactor initial setup

---
+++ setup/src/useFetch.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react'
+
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+
+export const useFetch = () => {
+  return <div>useFetch</div>
+}
